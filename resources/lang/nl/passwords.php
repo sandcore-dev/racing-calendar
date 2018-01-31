6f489acc230934c447a868f43552f9bdb8970750
Sander Verbrugge <32176563+sverbrugge@users.noreply.github.com>
2018-01-31 13:37:40 +0100

    Localization

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Het wachtwoord moet minstens 6 karakters bevatten en gelijk zijn aan de herhaling.',
+    'reset' => 'Uw wachtwoord is opnieuw ingesteld.',
+    'sent' => 'Er wordt een e-mail met uw wachtwoordherstel-link gestuurd.',
+    'token' => 'Deze wachtwoordherstel-link is ongeldig.',
+    'user' => "We kunnen geen gebruiker met dit e-mailadres vinden.",
+
+];
