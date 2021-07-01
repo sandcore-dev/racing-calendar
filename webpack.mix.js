04a55c8b8cab912fb57004d1a822cda6b4c10769
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2021-07-01 19:03:39 +0200

    Composer & NPM update; upgraded Laravel Mix to 6.x; added PHP 8.x as requirement

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
+mix.vue();
+
 if (mix.inProduction()) {
     mix.version();
 }
