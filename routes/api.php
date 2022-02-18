70f86bef7b2e57c1a4f0beea6f835af24902d87d
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2022-02-18 19:47:08 +0100

    Refactored routes to use methods and full class names

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\LocationsController;
+use Illuminate\Support\Facades\Route;
 
-Route::group([ 'middleware' => 'auth:api', 'namespace' => 'Api' ], function () {
-    Route::group([ 'prefix' => 'locations' ], function () {
-        Route::post('season/{season}', 'LocationsController@getBySeason');
-        Route::post('search', 'LocationsController@search');
+Route::middleware('auth:api')
+    ->group(function () {
+        Route::prefix('locations')
+            ->group(function () {
+                Route::post('season/{season}', [LocationsController::class, 'getBySeason']);
+                Route::post('search', [LocationsController::class, 'search']);
+            });
     });
-});
--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,73 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Admin;
+use App\Http\Controllers\CalendarController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
-// Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Auth::routes(
+    [
+        'register' => false,
+    ]
+);
 
-// Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::prefix('admin')
+    ->as('admin.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('championship', Admin\ChampionshipController::class)
+            ->except(['show', 'destroy']);
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => 'auth'], function () {
-    Route::resource('championship', 'ChampionshipController')
-        ->except(['show', 'destroy']);
+        Route::prefix('championship/{championship}')
+            ->group(function () {
+                Route::resource('season', Admin\SeasonController::class);
 
-    Route::prefix('championship/{championship}')
-        ->group(function () {
-            Route::resource('season', 'SeasonController');
+                Route::prefix('season/{season:id}')
+                    ->group(function () {
+                        Route::resource('race', Admin\RaceController::class);
 
-            Route::prefix('season/{season:id}')
-                ->group(function () {
-                    Route::resource('race', 'RaceController');
-                    Route::post('copy-season', 'RaceController@copySeason')->name('race.copy-season');
+                        Route::post('copy-season', [Admin\RaceController::class, 'copySeason'])
+                            ->name('race.copy-season');
 
-                    Route::prefix('race/{race:id}')
-                        ->as('race.')
-                        ->group(function () {
-                            Route::resource('session', 'RaceSessionController');
-                            Route::post('session/apply-template', 'RaceSessionController@applyTemplate')
-                                ->name('session.apply-template');
-                        });
-                });
-        });
+                        Route::prefix('race/{race:id}')
+                            ->as('race.')
+                            ->group(function () {
+                                Route::resource('session', Admin\RaceSessionController::class);
 
-    Route::resource('circuit', 'CircuitController');
-    Route::resource('country', 'CountryController');
-    Route::resource('location', 'LocationController');
-    Route::resource('template', 'TemplateController');
-    Route::resource('template.session', 'TemplateSessionController');
-});
+                                Route::post(
+                                    'session/apply-template',
+                                    [Admin\RaceSessionController::class, 'applyTemplate']
+                                )
+                                    ->name('session.apply-template');
+                            });
+                    });
+            });
+
+        Route::resource('circuit', Admin\CircuitController::class);
+        Route::resource('country', Admin\CountryController::class);
+        Route::resource('location', Admin\LocationController::class);
+        Route::resource('template', Admin\TemplateController::class);
+        Route::resource('template.session', Admin\TemplateSessionController::class);
+    });
 
 Route::domain('{championship:domain}')
     ->where(['championship' => '[a-z0-9.-]+'])
     ->group(function () {
-        Route::get('/', 'CalendarController@index')->name('index');
+        Route::get('/', [CalendarController::class, 'index'])
+            ->name('index');
 
-        Route::prefix('{season:access_token}')->group(function () {
-            Route::get('/', 'CalendarController@calendar')->name('calendar');
+        Route::prefix('{season:access_token}')
+            ->group(function () {
+                Route::get('/', [CalendarController::class, 'calendar'])
+                    ->name('calendar');
 
-            Route::prefix('{race:id}')->group(function () {
-                Route::get('location', 'CalendarController@editLocation')->name('calendar.location.edit');
-                Route::put('location', 'CalendarController@updateLocation')->name('calendar.location.update');
+                Route::prefix('{race:id}')
+                    ->group(function () {
+                        Route::get('location', [CalendarController::class, 'editLocation'])
+                            ->name('calendar.location.edit');
+
+                        Route::put('location', [CalendarController::class, 'updateLocation'])
+                            ->name('calendar.location.update');
+                    });
             });
-        });
     });
