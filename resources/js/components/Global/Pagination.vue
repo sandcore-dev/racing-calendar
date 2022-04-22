dd285c7d4b2610e4329bb242c73cc0d594a602ae
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2022-04-22 20:00:26 +0200

    Implemented Laravel/InertiaJS Vue pagination links component

---
+++ resources/js/components/Global/Pagination.vue
@@ -0,0 +1,84 @@
+<template>
+  <nav>
+    <ul class="pagination">
+      <li
+        class="page-item"
+        :class="getPageItemClass(links.first_page_url)"
+      >
+        <Link
+          :href="links.first_page_url"
+          class="page-link"
+        >
+          &laquo;
+        </Link>
+      </li>
+      <li :class="getPageItemClass(links.previous_page_url)">
+        <Link
+          :href="links.previous_page_url"
+          class="page-link"
+        >
+          &lt;
+        </Link>
+      </li>
+      <li
+        v-for="page in pages"
+        :key="page.label"
+        :class="getPageItemClass(page.active)"
+      >
+        <Link
+          :href="page.url"
+          class="page-link"
+        >
+          {{ page.label }}
+        </Link>
+      </li>
+      <li :class="getPageItemClass(links.next_page_url)">
+        <Link
+          :href="links.next_page_url"
+          class="page-link"
+        >
+          &gt;
+        </Link>
+      </li>
+      <li :class="getPageItemClass(links.last_page_url)">
+        <Link
+          :href="links.last_page_url"
+          class="page-link"
+        >
+          &raquo;
+        </Link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue';
+
+export default {
+    components: {
+        Link,
+    },
+
+    props: {
+        links: {
+            type: Array,
+            required: true,
+        },
+    },
+
+    computed: {
+        pages() {
+            return this.links.slice(1, -1);
+        },
+    },
+
+    methods: {
+        getPageItemClass(active) {
+            return active
+                ? 'page-item'
+                : 'page-item disabled';
+        },
+    },
+};
+</script>
