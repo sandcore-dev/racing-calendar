9b5353da420520a02df36f6d517885f7d5809600
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2022-07-25 00:21:40 +0200

    Refactored country create and edit Blade templates into Vue view components

---
+++ resources/js/views/Admin/Country/Create.vue
@@ -0,0 +1,58 @@
+<template>
+  <Form :data="data">
+    <template #default="{ form, getState }">
+      <b-form-group
+        :label="labels.code"
+        :state="getState(form.errors.code)"
+        :invalid-feedback="form.errors.code"
+      >
+        <b-form-select
+          v-model="form.code"
+          :state="getState(form.errors.code)"
+          :options="codes"
+          value-field="id"
+          text-field="name"
+        />
+      </b-form-group>
+    </template>
+  </Form>
+</template>
+
+<script>
+import {
+    BFormGroup,
+    BFormSelect,
+} from 'bootstrap-vue';
+
+import Form from '@/components/Form.vue';
+
+export default {
+    components: {
+        BFormGroup,
+        BFormSelect,
+
+        Form,
+    },
+
+    props: {
+        labels: {
+            type: Object,
+            required: true,
+        },
+
+        codes: {
+            type: Array,
+            required: true,
+        },
+
+        data: {
+            type: Object,
+            default() {
+                return {
+                    code: null,
+                };
+            },
+        },
+    },
+};
+</script>
---
+++ resources/js/views/Admin/Country/Edit.vue
@@ -0,0 +1,61 @@
+<template>
+  <Form :data="data">
+    <template #default="{ form, getState }">
+      <b-form-group
+        :label="labels.name"
+        :state="getState(form.errors.name)"
+        :invalid-feedback="form.errors.name"
+      >
+        <b-form-input
+          v-model="form.name"
+          :state="getState(form.errors.name)"
+        />
+      </b-form-group>
+      <b-form-group
+        :label="labels.code"
+        :state="getState(form.errors.code)"
+        :invalid-feedback="form.errors.code"
+      >
+        <b-form-input
+          v-model="form.code"
+          :state="getState(form.errors.code)"
+        />
+      </b-form-group>
+    </template>
+  </Form>
+</template>
+
+<script>
+import {
+    BFormGroup,
+    BFormInput,
+} from 'bootstrap-vue';
+
+import Form from '@/components/Form.vue';
+
+export default {
+    components: {
+        BFormGroup,
+        BFormInput,
+
+        Form,
+    },
+
+    props: {
+        labels: {
+            type: Object,
+            required: true,
+        },
+
+        data: {
+            type: Object,
+            default() {
+                return {
+                    name: null,
+                    code: null,
+                };
+            },
+        },
+    },
+};
+</script>
